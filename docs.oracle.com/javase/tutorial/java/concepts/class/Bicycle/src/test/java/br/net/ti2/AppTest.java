c1edb4c4e24b6039289a3f1c8a860d4f43c747b3
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-09 08:29:03 -0300

    init a new project

---
+++ docs.oracle.com/javase/tutorial/java/concepts/class/Bicycle/src/test/java/br/net/ti2/AppTest.java
@@ -0,0 +1,20 @@
+package br.net.ti2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
