c3434a45491d2b544b11073327474f0a1adaf4d0
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-08 22:29:13 -0300

    add source code comments

--- examples/java/HelloWorldApp.java
+++ examples/java/HelloWorldApp.java
@@ -1,3 +1,8 @@
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
 class HelloWorldAppp {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
