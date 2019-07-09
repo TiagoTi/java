0b2b00ba5645bcd72d32ffaeb648623512790245
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-08 22:57:30 -0300

    HelloWorldApp2.java:40: error: unclosed string literal

--- docs.oracle.com/javase/tutorial/getStarted/QandE/Exercises/Exercise2/HelloWorldApp2.java
+++ docs.oracle.com/javase/tutorial/getStarted/QandE/Exercises/Exercise2/HelloWorldApp2.java
@@ -37,7 +37,7 @@
  */
 class HelloWorldApp2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!); // Display the string.
+        System.out.println("Hello World!"); // Display the string.
     }
 }
 
