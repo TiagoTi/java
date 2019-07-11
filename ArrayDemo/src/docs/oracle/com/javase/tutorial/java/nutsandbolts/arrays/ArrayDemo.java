16abe841a5504a0416cf9f1c004ed20084fb92cc
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-11 07:10:44 -0300

    allocates memory for 10 integers

--- ArrayDemo/src/docs/oracle/com/javase/tutorial/java/nutsandbolts/arrays/ArrayDemo.java
+++ ArrayDemo/src/docs/oracle/com/javase/tutorial/java/nutsandbolts/arrays/ArrayDemo.java
@@ -3,8 +3,11 @@ package docs.oracle.com.javase.tutorial.java.nutsandbolts.arrays;
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		// declares an arry of int
+		// declares an array of integers
 		int[] myIntArray;
+
+		// allocates memory for 3 integers
+		myIntArray = new int[3];
 	}
 
 }
