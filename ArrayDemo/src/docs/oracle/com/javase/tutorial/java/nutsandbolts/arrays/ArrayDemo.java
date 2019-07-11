27992aad49af4fc4b298d9939956154fe621953a
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-11 07:07:54 -0300

    declares an int array

---
+++ ArrayDemo/src/docs/oracle/com/javase/tutorial/java/nutsandbolts/arrays/ArrayDemo.java
@@ -0,0 +1,10 @@
+package docs.oracle.com.javase.tutorial.java.nutsandbolts.arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// declares an arry of int
+		int[] myIntArray;
+	}
+
+}
