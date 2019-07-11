21d7443bb9aca7991c7454d50f5d67b8d82b34f8
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2019-07-11 07:15:50 -0300

    print each index element of array

--- ArrayDemo/src/docs/oracle/com/javase/tutorial/java/nutsandbolts/arrays/ArrayDemo.java
+++ ArrayDemo/src/docs/oracle/com/javase/tutorial/java/nutsandbolts/arrays/ArrayDemo.java
@@ -17,6 +17,10 @@ public class ArrayDemo {
 		
 		// initialize third element
 		myIntArray[2] = 300;
+		
+		System.out.println("Element at index 0: " + myIntArray[0] + ".\tFirst element!");
+		System.out.println("Element at index 1: " + myIntArray[1] + ".\tSecond element!");
+		System.out.println("Element at index 2: " + myIntArray[2] + ".\tThird element!");
 	}
 
 }
